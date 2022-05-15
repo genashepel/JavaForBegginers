f3c2b130c244b6cb26911b7d7cf4390138001e6c
Gena Shepel <genashepel@gmail.com>
2022-05-15 14:25:43 -0400

    HW10_Task_22

---
+++ src/homework10/HW10_Task_22.java
@@ -0,0 +1,45 @@
+package homework10;
+
+public class HW10_Task_22 {
+
+    public static String longestCommonSubstring(String one, String second, String third ){
+        if((one != null && second != null && third != null)
+                || (one.length() > 0 && second.length() > 0 && third.length() > 0)){
+
+            one = one.toLowerCase();
+            second = second.toLowerCase();
+            third = third.toLowerCase();
+
+            String smallestString = one;
+            String result = "";
+
+            if(smallestString.length() > second.length()){
+                smallestString = second;
+            }
+            if (smallestString.length() > third.length()){
+                smallestString = third;
+            }
+            String subString = "";
+            int j = 0;
+            for(int i = 0; i < smallestString.length(); i++){
+                subString = smallestString.substring(j, i + 1);
+                if(second.indexOf(subString) != - 1 && third.indexOf(subString) != -1){
+                    result =  subString;
+                } else {
+                    j++;
+                }
+
+            }
+
+            return result;
+        }
+
+        return "String is Empty or Null";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestCommonSubstring("Smaze", "amazing", "amazon"));
+        System.out.println(longestCommonSubstring("Smmazze", "mmazing", "ghMmammazon"));
+        System.out.println(longestCommonSubstring("Smmaaaazze", "mmaaaazing", "ghMmammazaaaaon"));
+    }
+}
